4b98d416fa452921c0d4657e9bc02c42e405b663
mahidhar reddy <mahidhar100008@gmail.com>
2025-07-01 06:08:21 +0530

    making common components an dreusing multiple places

---
+++ src/app/shared/button/button.component.ts
@@ -0,0 +1,15 @@
+import { Component, input } from '@angular/core';
+
+@Component({
+  selector: 'button[appButton]',
+  standalone: true,
+  imports: [],
+  templateUrl: './button.component.html',
+  styleUrl: './button.component.css',
+})
+export class ButtonComponent {
+  buttonData = input.required<{ text: string; icon: string }>();
+
+  text = () => this.buttonData().text;
+  icon = () => this.buttonData().icon;
+}
