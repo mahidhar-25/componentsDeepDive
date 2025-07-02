b101545acc0bef45385271416facdb05d14b813c
mahidhar reddy <mahidhar100008@gmail.com>
2025-07-02 06:06:02 +0530

    Extracting values based on template variable #titile

--- src/app/dashboard/server-status/server-status.component.ts
+++ src/app/dashboard/server-status/server-status.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-server-status',
@@ -7,6 +7,26 @@ import { Component } from '@angular/core';
   templateUrl: './server-status.component.html',
   styleUrl: './server-status.component.css',
 })
-export class ServerStatusComponent {
+export class ServerStatusComponent implements OnInit, OnDestroy {
   currentStatus = 'online';
+
+  constructor() {
+    // Simulate a status change after 3 seconds
+  }
+
+  private interval?: ReturnType<typeof setInterval>;
+  ngOnInit() {
+    this.interval = setInterval(() => {
+      const statuses = ['online', 'offline', 'unknown'];
+      this.currentStatus =
+        statuses[Math.floor(Math.random() * statuses.length)];
+    }, 2000);
+  }
+
+  ngOnDestroy() {
+    // Cleanup logic if needed
+    if (this.interval) {
+      clearInterval(this.interval);
+    }
+  }
 }
--- src/app/dashboard/tickets/new-ticket/new-ticket.component.ts
+++ src/app/dashboard/tickets/new-ticket/new-ticket.component.ts
@@ -1,11 +1,19 @@
 import { Component } from '@angular/core';
 import { ControlComponent } from '../../../shared/control/control.component';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-new-ticket',
   standalone: true,
-  imports: [ControlComponent],
+  imports: [ControlComponent, FormsModule],
   templateUrl: './new-ticket.component.html',
   styleUrl: './new-ticket.component.css',
 })
-export class NewTicketComponent {}
+export class NewTicketComponent {
+  onSubmit(title: string, request: string): void {
+    // Handle form submission logic here
+    console.log('Title:', title);
+    console.log('Request:', request);
+    console.log('Form submitted');
+  }
+}
